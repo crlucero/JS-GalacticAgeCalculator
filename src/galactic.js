c948084111a875f3ac9e3dda8218558d6f7bff16
Cristian Lucero <cristianjlucero32@gmail.com>
2019-01-11 11:52:38 -0800

    This commit adds a switch condition to check how much time a person may have life on mars depending on their gender and home continent

--- src/galactic.js
+++ src/galactic.js
@@ -33,4 +33,31 @@ export class Galactic {
         let ageOnJupiter = this.exactAge * 11.86;
         return Math.floor(ageOnJupiter);
     }
+
+   
+
+    // These functions determine how much life a person has left on Earth
+    // based on their current age, gender and what continet they live on.
+    // If 'male' is true use avg age of 78, else use avg age of 80. 
+    earthLifeExpectancy() {
+        switch(this.home) {
+            case "North America": 
+                return this.gender === "male" ? 77 : 81;
+
+            case "South America":
+                return this.gender === "male" ? 73 : 79;
+
+            case "Europe":
+                return this.gender === "male" ? 75 : 82;
+
+            case "Asia":
+                return this.gender === "male" ? 71 : 74;
+
+            case "Africa":
+                return this.gender === "male" ? 61 : 64;
+
+            case "Oceania":
+                return this.gender === "male" ? 76 : 80;
+        }
+    }
 }
